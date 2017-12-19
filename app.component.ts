bf204959aa00359fb0cf842f885a95559d9f8042
smitabh <smita5cool@gmail.com>
2017-12-19 23:34:30 +0530

    getList,editData,DeleteData

---
+++ app.component.ts
@@ -0,0 +1,104 @@
+
+import { Component ,Input, Output, EventEmitter,NgModule,OnInit} from '@angular/core';
+import { Router} from '@angular/router';
+import { empdataInfo} from  './Model/gridData';
+import { empService} from './Service/employeeSrvc.service';
+import { empdata } from './Model/data';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'app'; data1 = ''; 
+  nextLibAvailable: boolean= false;
+  isShow:boolean;
+  isHide:boolean;
+  emp:empdataInfo = {};
+  empList:empdataInfo[] = [];
+  salaryList =[];
+  toggoleShowHide:boolean;
+  disabled: boolean=false;
+  salaryTotal:any;
+  sucessmsg:string;
+  status:boolean = false;
+  deletemsg:string;
+ public genders = [
+    { value: 'F', display: 'Female' },
+    { value: 'M', display: 'Male' }
+ ];
+ constructor(private infoService:empService){}
+
+  ngOnInit()
+  {
+     this.getEmplList();
+     this.isHide = true;
+  }
+  
+  Show(data){
+    alert('hi');
+    this.data1 = data;
+    this.nextLibAvailable = false;
+  }
+getEmplList() {
+      this.infoService.getEmplList()
+          .subscribe(
+            Dataemp=> {
+               console.log(Dataemp)
+              this.empList = Dataemp;
+              for(let i=0; i < this.empList.length; i++){
+                this.empList[i].salary[0].salaryTot = this.empList[i].salary[0].basic + this.empList[i].salary[0].allowance - this.empList[i].salary[0].deduction;
+              }
+           },
+          err => {console.log(err); }); 
+    }
+    //edit data
+    editdata(editid)
+    {
+        this.emp = this.empList.filter(p=>p.id==editid)[0];
+       let Basic = this.emp.salary[0].basic;
+       let Allowance = this.emp.salary[0].allowance;
+       let Deduction = this.emp.salary[0].deduction;
+       let id = Basic + Allowance -Deduction ;
+    }
+    chngeval(data){
+      data.salary[0].salaryTot = parseInt(data.salary[0].basic) + parseInt(data.salary[0].allowance) - parseInt(data.salary[0].deduction);
+      this.sucessmsg = 'Salary updated successfully..!';
+      setTimeout(() => {
+        this.status = true;
+      },3000);
+    }
+   
+    clickMethod(data) {
+      if(confirm("Are you sure to edit this record "+data)) {
+        console.log("Implement edit functionality here");
+        this.nextLibAvailable = true;
+      }
+    }
+    deletedata(){
+      let i=0;
+      if(this.empList[i].empdel == true){
+        if(confirm("Are you sure to delete this record")) {
+          for(i=0; i < this.empList.length; i++){
+            if(this.empList[i].empdel && this.empList[i].empdel == true){
+              this.empList.splice(i,1);
+              i--;
+            }
+          }
+        }
+        this.deletemsg = 'Record deleted successfully..!';
+        setTimeout(() => {
+          this.status = true;
+        },3000);
+      }
+      else{
+        alert('Please select the checkbox');
+      }
+    }
+    
+}
